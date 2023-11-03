a18cef1ceaa5f91bdac8311010edfb10e39a308f
Fadil-Tao <hadadfadilah2354@gmail.com>
2023-11-03 21:06:24 +0800

    accessing file.txt method

--- src/Logic/Main.java
+++ src/Logic/Main.java
@@ -1,22 +1,44 @@
 package Logic;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class Main {
     public static void main(String[] args) {
+        String[] stringArray = new String[82];
         Tree tree = new Tree();
+        try {
+            String filename = "KataKunci.txt";
+            String absoluteFilePath = "";
+            String workingDir = System.getProperty("user.dir") + "\\src\\Source";
 
-       tree.add("Apple", "Apel");
-       tree.add("Biscuits" , "kue");
-       tree.add("Data" , "data");
-       tree.add("Nigeria", "Nigeria");
-       tree.add("Smart", "Pintar");
-       tree.add("world", "Dunia");
-       tree.add("Black", "Ireng");
-       tree.add("Brown", "coklat");
-       tree.add("Back", "kembali");
-       tree.add("Table" , "meja");
-       tree.add("Mouse" , "tikus");
-       tree.add("come" , "datang");
-       
-    
+            absoluteFilePath = workingDir + File.separator + filename;
+            // System.out.println(absoluteFilePath);
+            // System.out.println(new File(".").getAbsoluteFile());
+
+            FileReader fileReader = new FileReader(absoluteFilePath);
+            try (BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+                int i = 0 ;
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringArray[i] =  line.replaceAll("\\s", "");
+                    i++;
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        for (int i = 0; i < stringArray.length; i +=2) {
+            if (i + 1 < stringArray.length) {
+                tree.add(stringArray[i], stringArray[i+1]);
+            }
+        }
        tree.inorderTraversal(tree.getRoot());
+
     }
 }
